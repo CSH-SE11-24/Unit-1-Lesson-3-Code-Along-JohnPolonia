3fe6e272cdd9fed5c3e23093f682d355a6a8ed68
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-12 17:13:20 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// Code Along Norms: 
+// 1. Only be on Repl.it/classroom resources
+// 2. Actively follow along and engage
+// 3. Ask questions and flag if you get stuck
+// 4. Take notes as needed!
+let name = prompt("What's your name?")
+console.log("welcome to my game," + name)
+//String Interpolation - (`Welcome, ${adadad}`)
+let role = prompt("what role do you want? (magician, fighter, peasant")
+console.log(`Okay, ${name}, your role is now ${role}.`)
+
+let powerlevel = prompt("What is your power level?")
+
+powerlevel = parseInt(powerlevel)
+console.log(typeof powerlevel)
